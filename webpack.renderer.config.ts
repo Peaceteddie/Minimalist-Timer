14b769edb3e8c11f3fb184335d6fc19504992ac6
Peaceteddie <Peaceteddie@gmail.com>
2024-05-16 00:11:27 +0200

    fix: broken project state

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,39 +1,43 @@
-import type { Configuration } from 'webpack';
+import type { Configuration } from "webpack";
 
-import { rules } from './webpack.rules';
-import { plugins } from './webpack.plugins';
-
-import { app } from 'electron';
-import isDev from 'electron-is-dev';
+import { rules } from "./webpack.rules";
+import { plugins } from "./webpack.plugins";
 
 let fs;
-if (!isDev) {
-  fs = require('fs');
+if (process.env.NODE_ENV !== "development") {
+  fs = require("fs");
 }
 
 function getElectronPath() {
+  const { app } = require("electron");
+  const isDev = require("electron-is-dev");
+
   if (isDev) {
     return app.getAppPath();
   } else {
-    return app.getPath('exe');
+    return app.getPath("exe");
   }
 }
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [{ loader: "style-loader" }, { loader: "css-loader" }],
 });
 
 export const rendererConfig: Configuration = {
+  entry: "./src/index.ts",
   module: {
     rules,
   },
+  externals: {
+    electron: "commonjs electron",
+  },
   plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
     fallback: {
       fs: false,
-      path: require.resolve('path-browserify'),
+      path: require.resolve("path-browserify"),
     },
   },
 };
